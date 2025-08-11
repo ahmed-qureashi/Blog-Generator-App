b01f9d7e905fc7f158d09b3e347c71ca2637e8ab
ahmed-qureashi <ahmed@neusco.com>
2025-08-12 02:07:13 +0500

    adding logout functionality and requires sigin to view main page

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = 'login'  # URL to redirect to for login
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render , redirect
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
+def generate_blog(request):
+    pass
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -43,4 +46,5 @@ def user_signup(request):
 
     return render(request, 'signup.html')
 def user_logout(request):
-    pass   
+    logout(request) 
+    return redirect('/')  # Redirect to home page after logout
